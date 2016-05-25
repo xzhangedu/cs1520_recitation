2bbf2f98a06545c31c22635779227c446d808db7
Xiaozhong Zhang <xiaozhong@nietzsche.cs.pitt.edu>
2016-05-25 13:25:15 -0400

    more examples

--- ch4/ex4_1.js
+++
@@ -1,9 +0,0 @@
-var number, square, cube;
-
-document.write("Number, Square, Cube <br /><br />");
-
-for (number = 5; number < 16; number++)  {
-    square = number * number;
-    cube = number * square;
-    document.write(number + ",  " + square + ",  "  + cube + "<br />");
-}
